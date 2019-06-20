ca0cfb02db31d4bf5dbd082f42744269c63dd7d6
Freya <18758177547@163.com>
2019-06-20 22:58:18 +0800

    node服务器端加上jsonp

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+  }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,30 +23,44 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/') {
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.css">' +
-      '</head><body>'  +
-      '<h1>你好</h1>' +
-      '<script src="/main.js"></script>' +
-      '</body></html>')
-    response.end()
-  }else if(path === "/style.css"){
+
+    var string = fs.readFileSync("./index.html", "utf-8");
+    response.setHeader('Content-Type', 'text/html;charset=utf-8');
+    // response.write('<!DOCTYPE>\n<html>' +
+    //   '<head><link rel="stylesheet" href="/style.css">' +
+    //   '</head><body>' +
+    //   '<h1>你好</h1>' +
+    //   '<script src="/main.js"></script>' +
+    //   '</body></html>')
+    response.end(string);
+  } else if (path === "/style.css") {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
-  }else if(path == "/main.js"){
+  } else if (path == "/main.js") {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
     response.write('alert("这是JS执行的")')
     response.end()
-  }else{
+  } else if (path == "/pay") {
+    var num = fs.readFileSync("./money", "utf-8");
+    num = num - 1;
+    fs.writeFileSync("./money", num);
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+    var data = {
+      "code": 1,
+      "data": num
+    };
+    response.write(`${query.cbk}.call(undefined,${JSON.stringify(data)});`);
+    response.end();
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('呜呜呜')
+    response.write('页面丢失了')
     response.end()
   }
 
@@ -53,5 +69,3 @@ var server = http.createServer(function(request, response){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
